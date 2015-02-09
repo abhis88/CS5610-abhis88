efeb394502af9ab06f1f442246a60b563bc157fa
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-02-09 06:52:10 -0500

    Exp2 added
    Adding Exp2

---
+++ experiments/week3/jQueryUI/js/Exp2.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $(".p1").resizable({
+        handles: "n, e, w, s, se, sw, ne, nw"
+    });
+    $(".p2").draggable({
+        grid: [20, 20]
+    });
+    
+    $(".square").each(function () {
+        $(this).resizable({
+            handles: $(this).attr("id")
+        });
+    });
+})
