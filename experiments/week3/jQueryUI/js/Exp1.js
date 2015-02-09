1e46fe445bce751b6b2b6ad7afc59b1a5fda8be7
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-02-09 05:13:12 -0500

    Update Exp 1
    Minor modification to Exp 1

--- experiments/week3/jQueryUI/js/Exp1.js
+++ experiments/week3/jQueryUI/js/Exp1.js
@@ -2,7 +2,9 @@
     $("#p1").draggable();
     $("#p2").draggable({
         grid: [20, 20]
-    })
+    });
     $("#p3").draggable();
-    $(".div1, .div2").draggable();
+    $(".div1, .div2").draggable({
+        grid: [20, 20]
+    });
 })
