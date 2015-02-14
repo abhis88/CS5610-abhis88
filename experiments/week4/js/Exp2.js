c80e24687f399fdaaa96edbdd9405f1adfff6d76
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-02-14 15:44:28 -0500

    Week 4 and Exp 2
    Week 4 and Exp 2

---
+++ experiments/week4/js/Exp2.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("firstController", [])
+
+app.controller("Controller",
+    function ($scope) {
+        var employee = {
+            firstName: "",
+            lastName: "",
+            mobile: "",
+            county: "",
+            address:"",
+        };
+
+        $scope.employee = employee;
+
+    });
