a4ff21c618e77b0425221e8f8f0f8a1127ab060b
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-02-14 17:35:09 -0500

    Week 4 Exp 3
    Week 4 Exp 3

---
+++ experiments/week4/js/Exp3.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module("cal", [])
+
+app.controller("Controller-Cal", function ($scope) {
+
+    $scope.sum = function () {
+        var i1 = $scope.input1;
+        var i2 = $scope.input2;
+        $scope.result = parseInt(i1) + parseInt(i2);
+    }
+
+    $scope.minus = function () {
+        var i1 = $scope.input1;
+        var i2 = $scope.input2;
+        $scope.result = parseInt(i1) - parseInt(i2);
+    }
+
+    $scope.multiply = function () {
+        var i1 = $scope.input1;
+        var i2 = $scope.input2;
+        $scope.result = parseInt(i1) * parseInt(i2);
+    }
+
+    $scope.divide = function () {
+        var i1 = $scope.input1;
+        var i2 = $scope.input2;
+        $scope.result = parseInt(i1) / parseInt(i2);
+    }
+});
