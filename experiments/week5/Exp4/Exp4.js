903e079fdf3f273437a352b69ac6f15a880d7d11
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-02-22 14:30:22 -0500

    Week 5 Exp 4 and Exp 5
    Week 5 Exp 4 and Exp 5
    Forgot to upload

---
+++ experiments/week5/Exp4/Exp4.js
@@ -0,0 +1,92 @@
+﻿var app = angular.module("CourseApp", ["ngRoute"]);
+
+app.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+          when('/home', {
+              templateUrl: 'course/home.html'
+          }).
+          when('/profile/:username', {
+              templateUrl: 'course/profile.html',
+              controller: 'ProfileController'
+          }).
+          when('/contact', {
+              templateUrl: 'course/contact.html',
+              controller: 'ContactController'
+          }).
+          when('/about', {
+              templateUrl: 'course/about.html',
+              controller: 'AboutController'
+          }).
+          otherwise({
+              redirectTo: 'home'
+          });
+    }]);
+
+app.controller("ProfileController", function ($scope, UserService, $routeParams) {
+    var username = $routeParams.username;
+    $scope.username = username;
+    $scope.currentUser = UserService.getCurrentUser();
+});
+
+app.controller("ContactController", function ($scope, UserService, $routeParams) {
+    var username = $routeParams.username;
+    $scope.username = username;
+    $scope.currentUser = UserService.getCurrentUser();
+});
+
+app.controller("AboutController", function ($scope, UserService, $routeParams) {
+    var username = $routeParams.username;
+    $scope.username = username;
+    $scope.currentUser = UserService.getCurrentUser();
+});
+
+app.controller("navController", function ($scope, UserService) {
+    $scope.login = function () {
+        $scope.currentUser = null;
+        $scope.logout = function () {
+            $scope.currentUser = null;
+            UserService.logout();
+            $scope.username = null;
+            $scope.password = null;
+        }
+
+        var username = $scope.username;
+        var password = $scope.password;
+        $scope.currentUser = UserService.login(username, password);
+    }
+});
+
+app.factory("UserService", function () {
+    var currentUser = null;
+
+    var users = [
+        { username: "admin", password: "admin", role: "admin" },
+        { username: "faculty", password: "faculty", role: "faculty" },
+        { username: "student", password: "student", role:"student" }
+    ]
+    
+    var logout = function () {
+        currentUser = null;
+    };
+
+    var login = function (username, password) {
+        for (var u in users) {
+            if (users[u].username == username) {
+                currentUser = users[u];
+                return users[u];
+            }
+        }
+        return null;
+    };
+
+    var getCurrentUser = function () {
+        return currentUser;
+    }
+
+    return {
+        login: login,
+        logout: logout,
+        getCurrentUser: getCurrentUser
+    }
+});
---
+++ experiments/week5/Exp5/Exp5.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("TextApp", []);
+
+app.controller("AppController", function ($scope) {
+    $scope.blogs = [];
+    $scope.message = "";
+
+    $scope.left = function () {
+        var rem = 100 - $scope.message.length;
+        return rem;
+    };
+
+    $scope.clearMessage = function () {
+        $scope.message = "";
+    };
+
+    $scope.saveBlog = function () {
+        $scope.currentDate = new Date();
+        var blog = {
+            title: $scope.message,
+            date: $scope.currentDate
+        }
+        $scope.blogs.push(blog);
+    };
+
+    $scope.removeBlog = function (blog) {
+        var index = $scope.blogs.indexOf(blog);
+        $scope.blogs.splice(index, 1);
+    }
+});
