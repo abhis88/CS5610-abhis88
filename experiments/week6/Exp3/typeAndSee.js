d603d8b513ef8b067ba18cb0580d7dd7a6d8334d
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-03-01 06:33:49 -0500

    Week 6 Experiment 2, 3 and blog
    Node Js and Open Shift Experiments.

---
+++ experiments/week6/Exp3/typeAndSee.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $("#click1").click(function () {
+        var link1 = $("#input1").val();
+        location.href = "http://nodejs-abhis88.rhcloud.com/api/Exp3/" + link1;
+    });
+})
+
