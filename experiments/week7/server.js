520efe692a73112c3d4c008aaeba8a4c602a04c3
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-03-09 16:38:38 -0400

    Week 7 Exp 6
    Week 7 Exp 6

--- experiments/week7/server.js
+++ experiments/week7/server.js
@@ -4,41 +4,35 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/week7');
+var connectionString = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/experiments'
+mongoose.connect(connectionString);
 
 
-var WebsiteSchema = new mongoose.Schema({
-    name: String
-}, { collection: 'website' });
+var studentSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    school: String,
+    location: String,
+    mobile: String,
+    country: String,
+    homeCountry: String,
+    sport: String,
+    age: String,
+    weight: String
+}, { collection: 'student' });
 
-var WebsiteModel = mongoose.model('WebsiteModel', WebsiteSchema);
+var playerSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    speciality: String
+}, { collection: 'player' });
 
-var site1 = new WebsiteModel({ name: 'Site 1' });
-site1.save();
-
-
-
-app.get("/api/Week7/Exp1", function (req, res) {
-    var sampleData = {
-        firstName: "Abhishek",
-        lastName: "Kumar",
-        school: "Northeastern University",
-        location: "Boston",
-        mobile: "8572348869",
-        country: "USA",
-        homeCountry: "India",
-        sport: "Cricket",
-        age: "26",
-        weight: "80 Kg"
-    };
-    res.json(sampleData);
-});
-
+var studentModel = mongoose.model('studentModel', studentSchema);
+var playerModel = mongoose.model('playerModel', playerSchema);
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
-
 
 app.use(express.static(__dirname + '/public'));
 
@@ -99,17 +93,37 @@ var players = [
     }
 ];
 
+app.get("/Week7_Exp6.html/data", function (req, res) {
+    playerModel.find(function (err, data) {
+        res.json(data);
+    });
+});
+
+app.post("/Week7_Exp6.html/data", function (req, res) {
+    var player1 = new playerModel(req.body);
+    player1.save(function(err, doc){
+        playerModel.find(function (err, data) {
+            res.json(data);
+        });
+    });    
+});
+
+app.delete("/Week7_Exp6.html/data/:id", function (req, res) {
+    playerModel.findById(req.params.id, function(err, doc){
+        doc.remove();
+        playerModel.find(function (err, data) {
+            res.json(data);
+        });
+    });
+});
+
 app.delete("/api/player/:id", function (req, res) {
     players.splice(req.params.id, 1);
     res.json(players);
-})
+});
 
 app.get("/api/player", function (req, res) {
     res.json(players);
-})
-
-app.get("/api/player/:id", function (req, res) {
-    res.json(players[req.params.id]);
 })
 
 app.post("/api/player", function (req, res) {
@@ -117,10 +131,14 @@ app.post("/api/player", function (req, res) {
     res.json(players);
 })
 
+app.get("/api/player/:id", function (req, res) {
+    res.json(players[req.params.id]);
+})
+
 app.put("/api/player/:id", function (req, res) {
     players[req.params.id] = req.body;
     res.json(players);
-})
+});
 
 
 /* Simple Hello World Experiment*/
@@ -151,8 +169,81 @@ app.get("/api/Exp4/:var", function (req, res) {
     res.json(coursesTaken[req.params.var]);
 });
 
-app.get("/process", function (req, res) {
-    res.json(process.evn);
+
+app.get("/api/Week7/Exp2", function (req, res) {
+    var sampleData = {
+        firstName: "Abhishek",
+        lastName: "Kumar",
+        school: "Northeastern University",
+        location: "Boston",
+        mobile: "8572348869",
+        country: "USA",
+        homeCountry: "India",
+        sport: "Cricket",
+        age: "26",
+        weight: "80 Kg"
+    };
+    res.json(sampleData);
+});
+
+
+app.get('/api/Week7/Exp3', function (req, res) {
+    studentModel.find(function (err, data) {
+        res.json(data);
+    });
+});
+
+app.get('/api/Week7/Exp3/firstName/:firstName', function (req, res) {
+    studentModel.find({ firstName: { "$in": [req.params.firstName] } }, function (err, data) {
+        res.json(data);
+    });
+});
+
+app.get('/api/Week7/Exp3/location/:location', function (req, res) {
+    studentModel.find({ location: { "$in": [req.params.location] } }, function (err, data) {
+        res.json(data);
+    });
+});
+
+app.get('/api/Week7/Exp3/school/:school', function (req, res) {
+    studentModel.find({ school: { "$in": [req.params.school] } }, function (err, data) {
+        res.json(data);
+    });
+});
+
+app.get('/api/Week7/Exp4', function (req, res) {
+    studentModel.count(function (err, data) {
+        res.json("Count of the rows is " + data);
+    });
+});
+
+app.get('/api/Week7/Exp5/location/:location', function (req, res) {
+    studentModel.remove({ location: { "$in": [req.params.location] } }, function (err, data) {
+        res.json("Number of records deleted : " + data);
+    });
+});
+
+app.get('/api/Week7/Exp5/firstName/:firstName', function (req, res) {
+    studentModel.remove({ firstName: { "$in": [req.params.firstName] } }, function (err, data) {
+        res.json("Number of records deleted : " + data);
+    });
+});
+
+app.get('/api/Week7/LoadData', function (req, res) {
+    var student1 = new studentModel({ firstName: 'Abhishek', lastName: 'Kumar', school: 'Northeastern University', location: 'Boston', mobile: '8572348869', country: 'USA', homeCountry: 'India', sport: 'Cricket', age: '26', weight: '80 Kg' });
+    var student2 = new studentModel({ firstName: 'Shakti', lastName: 'Patro', school: 'Northeastern University', location: 'Boston', mobile: '89999998998', country: 'USA', homeCountry: 'India', sport: 'Table Tennis', age: '27', weight: '85 Kg' });
+    var student3 = new studentModel({ firstName: 'Vikas', lastName: 'Shriyan', school: 'Northeastern University', location: 'Boston', mobile: '9868686568', country: 'USA', homeCountry: 'India', sport: 'Cricket', age: '24', weight: '75 Kg' });
+    var student4 = new studentModel({ firstName: 'Kartik', lastName: 'Mahaley', school: 'Northeastern University', location: 'Boston', mobile: '123456789', country: 'USA', homeCountry: 'India', sport: 'Cricket', age: '30', weight: '60 Kg' });
+    var student5 = new studentModel({ firstName: 'Pankaj', lastName: 'Tripathi', school: 'Northeastern University', location: 'Boston', mobile: '987654321', country: 'USA', homeCountry: 'India', sport: 'Lawn Tennis', age: '25', weight: '80 Kg' });
+
+    student1.save();
+    student2.save();
+    student3.save();
+    student4.save();
+    student5.save();
+
+    res.json("Data loaded successfully");
+
 });
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
