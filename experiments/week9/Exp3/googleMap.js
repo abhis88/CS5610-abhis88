9ebc35c974aa7df3200ac0c6213a15654030def7
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-03-22 10:43:41 -0400

    Week 9 Blog and Experiment 5
    Week 9 Blog and Experiment 5

---
+++ experiments/week9/Exp3/googleMap.js
@@ -0,0 +1,17 @@
+﻿
+function initialize() {
+    var googleMap = {
+        zoom: 2, center: new google.maps.LatLng(49.265984, -123.127491)
+    };
+
+    var map1 = new google.maps.Map(document.getElementById('map-canvas-2'), googleMap);
+
+    var weatherLayer = new google.maps.weather.WeatherLayer({
+        temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
+    });
+
+    weatherLayer.setMap(map1);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
