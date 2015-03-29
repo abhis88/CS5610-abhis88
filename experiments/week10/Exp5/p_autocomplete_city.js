a630626b63736cec78081fec4d21397600111104
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-03-29 10:35:52 -0400

    Week 10 - Project Related Experiments
    Week 10 - Project Related Experiments

---
+++ experiments/week10/Exp5/p_autocomplete_city.js
@@ -0,0 +1,27 @@
+﻿jQuery(function () 
+{
+    jQuery("#source").autocomplete({
+        source: function (request, response) {
+            jQuery.getJSON(
+               "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
+               function (data) {
+                   response(data);
+               }
+            );
+        },
+        minLength: 3,
+        select: function (event, ui) {
+            var selectedObj = ui.item;
+            jQuery("#source").val(selectedObj.value);
+            getcitydetails(selectedObj.value);
+            return false;
+        },
+        open: function () {
+            jQuery(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+        },
+        close: function () {
+            jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+        }
+    });
+    jQuery("#source").autocomplete("option", "delay", 100);
+});
---
+++ experiments/week10/Exp6/p_json_object.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+
+    $("#getDetails").click(function () {
+
+        var source = $("#source").val();
+        var destination = $("#destination").val();
+
+        $.ajax({
+            url: "http://free.rome2rio.com/api/1.2/json/Search?key=eU7G6lzt&oName=" + source + "&dName=" + destination,
+            cache: "false",
+            success: function (data) {
+                console.log(data);
+                alert(data);
+            }
+        });
+    });
+});
