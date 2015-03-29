9cb1484159e15cf4e1a237eb657229d487f39595
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-03-29 11:20:50 -0400

    Week 10 - Project Experiment 5
    Week 10 - Project Experiment 5

--- experiments/week10/Exp5/p_autocomplete_city.js
+++ experiments/week10/Exp5/p_autocomplete_city.js
@@ -1,5 +1,7 @@
 ﻿jQuery(function () 
 {
+    /* --- SOURCE ---*/
+
     jQuery("#source").autocomplete({
         source: function (request, response) {
             jQuery.getJSON(
@@ -23,5 +25,31 @@
             jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
         }
     });
-    jQuery("#source").autocomplete("option", "delay", 100);
+
+    /*  --- DESTINATION --- */
+
+    jQuery("#destination").autocomplete({
+        source: function (request, response) {
+            jQuery.getJSON(
+               "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=" + request.term,
+               function (data) {
+                   response(data);
+               }
+            );
+        },
+        minLength: 3,
+        select: function (event, ui) {
+            var selectedObj = ui.item;
+            jQuery("#destination").val(selectedObj.value);
+            getcitydetails(selectedObj.value);
+            return false;
+        },
+        open: function () {
+            jQuery(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+        },
+        close: function () {
+            jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+        }
+    });
+    jQuery("#source, #destination").autocomplete("option", "delay", 100);
 });
