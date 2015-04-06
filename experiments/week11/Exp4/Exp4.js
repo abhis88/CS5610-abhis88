008033ee4f462cc81aeed29446a60d88cb112406
Abhishek Kumar <kumar.abh@husky.neu.edu>
2015-04-05 20:09:10 -0400

    Week 11 Experiment 4
    Week 11 Experiment 4

---
+++ experiments/week11/Exp4/Exp4.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("TravelApp", []);
+
+app.controller("Travel-Controller",
+    function ($scope, $http) {
+
+        var airports = [];
+
+        $scope.searchAirports = function () {
+
+            var oCity = $scope.oCity;
+            var dCity = $scope.dCity;
+
+            $http.get("http://free.rome2rio.com/api/1.2/json/Search?key=eU7G6lzt&oName=" + oCity + "&dName=" + dCity)
+            .success(function (response) {
+                console.log(response);
+                $scope.airports = response.airports;
+            })
+        }
+    });
